15cd4dcf74928819809c71fc0b81c85e6db9b8c9
Frank Warmerdam <warmerdam@pobox.com>
2002-11-28 15:34:45 +0000

    changed to dump EPSG 6.2.2 tables
    git-svn-id: https://svn.osgeo.org/metacrs/geotiff/trunk@455 4e78687f-474d-0410-85f9-8d5e500ac6b2

--- geotiff/libgeotiff/csv/odbc2csv/dumpepsg.bat
+++ geotiff/libgeotiff/csv/odbc2csv/dumpepsg.bat
@@ -1,3 +1,3 @@
-odbc2csv compd_cs ellips_alias ellipsoid gdatum_alias geod_datum geod_trf horiz_cs p_meridian trf_method trf_nonpolynomial trf_path uom_an_alias uom_angle uom_le_alias uom_length uom_sc_alias uom_scale vert_cs vert_datum vert_offset
 
-odbc2csv -c compd_cs ellips_alias ellipsoid gdatum_alias geod_datum geod_trf horiz_cs p_meridian trf_method trf_nonpolynomial trf_path uom_an_alias uom_angle uom_le_alias uom_length uom_sc_alias uom_scale vert_cs vert_datum vert_offset
+odbc2csv -ds EPSG622 "Coordinate Reference System" "Coordinate Axis" "Coordinate Axis Name" "Coordinate System" Coordinate_Operation "Coordinate_Operation Method" "Coordinate_Operation Parameter" "Coordinate_Operation Parameter Value" "Coordinate_Operation Path" Datum Ellipsoid "Prime Meridian" "Unit of Measure" Alias Area Change Codes Deprecation "Naming System" "Version History"
+
