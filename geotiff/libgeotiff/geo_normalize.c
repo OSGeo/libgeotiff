e86ff256c8fcb5475f0f6c38573030fe204f2ea8
Frank Warmerdam <warmerdam@pobox.com>
2003-01-15 04:39:16 +0000

    Added GTIFDeaccessCSV
    git-svn-id: https://svn.osgeo.org/metacrs/geotiff/trunk@493 4e78687f-474d-0410-85f9-8d5e500ac6b2

--- geotiff/libgeotiff/geo_normalize.c
+++ geotiff/libgeotiff/geo_normalize.c
@@ -28,6 +28,9 @@
  ******************************************************************************
  *
  * $Log$
+ * Revision 1.35  2003/01/15 04:39:16  warmerda
+ * Added GTIFDeaccessCSV
+ *
  * Revision 1.34  2003/01/15 03:37:40  warmerda
  * added GTIFFreeMemory()
  *
@@ -2306,4 +2309,16 @@ void GTIFFreeMemory( char * pMemory )
 {
     if( pMemory != NULL )
         VSIFree( pMemory );
+}
+
+/************************************************************************/
+/*                          GTIFDeaccessCSV()                           */
+/*                                                                      */
+/*      Free all cached CSV info.                                       */
+/************************************************************************/
+
+void GTIFDeaccessCSV()
+
+{
+    CSVDeaccess( NULL );
 }
