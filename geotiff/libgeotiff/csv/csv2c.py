fdccd6674f2ac01a984d8afadd7f2429dbeb0a34
Frank Warmerdam <warmerdam@pobox.com>
2004-03-20 07:10:21 +0000

    New
    git-svn-id: https://svn.osgeo.org/metacrs/geotiff/trunk@576 4e78687f-474d-0410-85f9-8d5e500ac6b2

---
+++ geotiff/libgeotiff/csv/csv2c.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+#******************************************************************************
+#  $Id$
+# 
+#  Project:  GDAL
+#  Purpose:  Build the epsg_pcs.csv table with the minimum information
+#            required to define a PCS (not including GCS defs). 
+#  Author:   Frank Warmerdam, warmerdam@pobox.com
+#******************************************************************************
+#  Copyright (c) 2002, Frank Warmerdam <warmerdam@pobox.com>
+# 
+#  Permission is hereby granted, free of charge, to any person obtaining a
+#  copy of this software and associated documentation files (the "Software"),
+#  to deal in the Software without restriction, including without limitation
+#  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+#  and/or sell copies of the Software, and to permit persons to whom the
+#  Software is furnished to do so, subject to the following conditions:
+# 
+#  The above copyright notice and this permission notice shall be included
+#  in all copies or substantial portions of the Software.
+# 
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+#  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+#  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#  DEALINGS IN THE SOFTWARE.
+#******************************************************************************
+# 
+# $Log$
+# Revision 1.1  2004/03/20 07:10:21  warmerda
+# New
+#
+#
+
+import string
+import sys
+import csv_tools
+
+###############################################################################
+def emit_token_line( fd, field_list, counter, varname ):
+    fd.write( 'datafile_rows_t %s_row_%d[] = {' % (varname, counter) )
+    for item in field_list:
+        item = string.replace(item,'\n',' ')
+        item = string.replace(item,'"','\\\"')
+        fd.write( '"%s",' % item )
+    fd.write( 'NULL};\n' )
+    
+###############################################################################
+def convert_csv_table( csv_filename ):
+    c_filename = csv_filename[0:-3] + 'c'
+    varname = csv_filename[0:-4]
+
+    lines = csv_tools.merge_split_lines(open(csv_filename).readlines())
+
+    c_fd = open( c_filename, 'w' )
+    c_fd.write( '#include "defs.h"\n' )
+
+    data_lines = 0
+    for line in lines:
+        emit_token_line( c_fd, csv_tools.SplitCSVLine(line), data_lines,
+                         varname )
+        data_lines = data_lines + 1
+
+    c_fd.write( 'datafile_rows_t *%s_rows[] = {' % varname )
+    for i in range(data_lines):
+        if i != 0:
+            c_fd.write(' ,')
+        c_fd.write( '%s_row_%d' % (varname, i) )
+    c_fd.write( '};\n' )
+    c_fd = None
+
+###############################################################################
+# main
+if len(sys.argv) < 2:
+    print 'Usage: csv2c.py filename.csv [...]'
+    sys.exit( 1 )
+
+for arg in sys.argv[1:]:
+    convert_csv_table( arg )
