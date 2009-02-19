681511e8403a6ef16dad8d41547bef2532509808
Frank Warmerdam <warmerdam@pobox.com>
2009-02-19 03:13:02 +0000

    compute count for ascii tag values
    git-svn-id: https://svn.osgeo.org/metacrs/geotiff/trunk@1531 4e78687f-474d-0410-85f9-8d5e500ac6b2

--- libgeotiff/geo_simpletags.c
+++ libgeotiff/geo_simpletags.c
@@ -175,6 +175,14 @@ int ST_SetKey( ST_TIFF *st, int tag, int count, int st_type, void *data )
     int i, item_size = ST_TypeSize( st_type );
 
 /* -------------------------------------------------------------------- */
+/*      We should compute the length if we were not given a count       */
+/* -------------------------------------------------------------------- */
+    if (count == 0 && st_type == STT_ASCII ) 
+    {
+        count = strlen((char*)data);
+    }
+
+/* -------------------------------------------------------------------- */
 /*      If we already have a value for this tag, replace it.            */
 /* -------------------------------------------------------------------- */
     for( i = 0; i < st->key_count; i++ )
