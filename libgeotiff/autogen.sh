cf4cb63d271f8a75d2fc255f6c0d6495d78fec7f
Frank Warmerdam <warmerdam@pobox.com>
2009-09-24 23:31:19 +0000

    restore more complete autogen while preserving geo_config.h.in (#10)
    git-svn-id: https://svn.osgeo.org/metacrs/geotiff/trunk@1650 4e78687f-474d-0410-85f9-8d5e500ac6b2

--- libgeotiff/autogen.sh
+++ libgeotiff/autogen.sh
@@ -11,14 +11,37 @@ giveup()
         echo
         echo "  Something went wrong, giving up!"
         echo
+        cp geo_config.h.in.safe geo_config.h.in
         exit 1
 }
 
+cp geo_config.h.in geo_config.h.in.safe
+OSTYPE=`uname -s`
 
+for libtoolize in glibtoolize libtoolize; do
+    LIBTOOLIZE=`which $libtoolize 2>/dev/null`
+    if test "$LIBTOOLIZE"; then
+        break;
+    fi
+done
+
+#AMFLAGS="--add-missing --copy --force-missing"
+AMFLAGS="--add-missing --copy"
+if test "$OSTYPE" = "IRIX" -o "$OSTYPE" = "IRIX64"; then
+   AMFLAGS=$AMFLAGS" --include-deps";
+fi
+
+echo "Running aclocal"
+aclocal || giveup
+#echo "Running autoheader"
+#autoheader || giveup
+echo "Running libtoolize"
+$LIBTOOLIZE --force --copy || giveup
 echo "Running automake"
 automake $AMFLAGS # || giveup
 echo "Running autoconf"
 autoconf || giveup
+cp geo_config.h.in.safe geo_config.h.in
 
 echo "======================================"
 echo "Now you are ready to run './configure'"
