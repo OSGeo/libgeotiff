74c3dcda24dea9d05a3f62baf17f9a4ad782f17e
Howard Butler <hobu.inc@gmail.com>
2024-05-21 14:32:55 -0500

    attempt to modernize cmake a bit

--- libgeotiff/cmake/FindGeoTIFF.cmake
+++ libgeotiff/cmake/FindGeoTIFF.cmake
@@ -5,12 +5,16 @@
 # On success, the macro sets the following variables:
 # GEOTIFF_FOUND       = if the library found
 # GEOTIFF_LIBRARIES   = full path to the library
-# GEOTIFF_INCLUDE_DIR = where to find the library headers 
+# GEOTIFF_INCLUDE_DIR = where to find the library headers
 # also defined, but not for general use are
-# GEOTIFF_LIBRARY, where to find the PROJ.4 library.
+# GEOTIFF_LIBRARY
 #
 # Copyright (c) 2009 Mateusz Loskot <mateusz@loskot.net>
+# Copyright (c) 2016 NextGIS <info@nextgis.com>
+# Copyright (C) 2017,2018 Hiroshi Miura
 #
+# Origin from
+# https://svn.osgeo.org/metacrs/geotiff/trunk/libgeotiff/cmake/FindGeoTIFF.cmake
 # Module source: http://github.com/mloskot/workshop/tree/master/cmake/
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
@@ -18,54 +22,83 @@
 #
 ###############################################################################
 
-SET(GEOTIFF_NAMES geotiff)
+find_path(GEOTIFF_INCLUDE_DIR geotiff.h PATH_SUFFIXES geotiff libgeotiff)
 
-IF(WIN32)
+set(GEOTIFF_NAMES ${GEOTIFF_NAMES} geotiff geotiff_i)
+set(GEOTIFF_NAMES_DEBUG ${GEOTIFF_NAMES_DEBUG} geotiffd geotiff_d geotiff_d_i)
 
-    IF(MINGW)
-        FIND_PATH(GEOTIFF_INCLUDE_DIR
-            geotiff.h
-            PATH_PREFIXES geotiff
-            PATHS
-            /usr/local/include
-            /usr/include
-            c:/msys/local/include)
+if(NOT GEOTIFF_LIBRARY)
+  find_library(GEOTIFF_LIBRARY_RELEASE NAMES ${GEOTIFF_NAMES})
+  find_library(GEOTIFF_LIBRARY_DEBUG NAMES ${GEOTIFF_NAMES_DEBUG})
+  include(SelectLibraryConfigurations)
+  select_library_configurations(GEOTIFF)
+  mark_as_advanced(GEOTIFF_LIBRARY_RELEASE GEOTIFF_LIBRARY_DEBUG)
+endif()
 
-        FIND_LIBRARY(GEOTIFF_LIBRARY
-            NAMES ${GEOTIFF_NAMES}
-            PATHS
-            /usr/local/lib
-            /usr/lib
-            c:/msys/local/lib)
-    ENDIF(MINGW)
+unset(GEOTIFF_NAMES)
+unset(GEOTIFF_NAMES_DEBUG)
 
-    IF(MSVC)
-        SET(GEOTIFF_INCLUDE_DIR "$ENV{LIB_DIR}/include" CACHE STRING INTERNAL)
 
-        SET(GEOTIFF_NAMES ${GEOTIFF_NAMES} geotiff_i)
-        FIND_LIBRARY(GEOTIFF_LIBRARY NAMES 
-            NAMES ${GEOTIFF_NAMES}
-            PATHS
-            "$ENV{LIB_DIR}/lib"
-            /usr/lib
-            c:/msys/local/lib)
-    ENDIF(MSVC)
-  
-ELSEIF(UNIX)
+if(GEOTIFF_INCLUDE_DIR)
+    set(GEOTIFF_MAJOR_VERSION 0)
+    set(GEOTIFF_MINOR_VERSION 0)
+    set(GEOTIFF_PATCH_VERSION 0)
+    set(GEOTIFF_REVISION_VERSION 0)
 
-    FIND_PATH(GEOTIFF_INCLUDE_DIR geotiff.h PATH_PREFIXES geotiff)
+    if(EXISTS "${GEOTIFF_INCLUDE_DIR}/geotiff.h")
+        file(READ "${GEOTIFF_INCLUDE_DIR}/geotiff.h" GEOTIFF_H_CONTENTS)
+        string(REGEX MATCH "LIBGEOTIFF_VERSION[ \t]+([0-9]+)"
+          LIBGEOTIFF_VERSION ${GEOTIFF_H_CONTENTS})
+        string (REGEX MATCH "([0-9]+)"
+          LIBGEOTIFF_VERSION ${LIBGEOTIFF_VERSION})
 
-    FIND_LIBRARY(GEOTIFF_LIBRARY NAMES ${GEOTIFF_NAMES})
+        string(SUBSTRING ${LIBGEOTIFF_VERSION} 0 1 GEOTIFF_MAJOR_VERSION)
+        string(SUBSTRING ${LIBGEOTIFF_VERSION} 1 1 GEOTIFF_MINOR_VERSION)
+        string(SUBSTRING ${LIBGEOTIFF_VERSION} 2 1 GEOTIFF_PATCH_VERSION)
+        string(SUBSTRING ${LIBGEOTIFF_VERSION} 3 1 GEOTIFF_REVISION_VERSION)
 
-ELSE()
-    MESSAGE("FindGeoTIFF.cmake: unrecognized or unsupported operating system")
-ENDIF()
+        unset(GEOTIFF_H_CONTENTS)
+    endif()
 
-IF(GEOTIFF_FOUND)
-  SET(GEOTIFF_LIBRARIES ${GEOTIFF_LIBRARY})
-ENDIF()
+    set(GEOTIFF_VERSION_STRING "${GEOTIFF_MAJOR_VERSION}.${GEOTIFF_MINOR_VERSION}.${GEOTIFF_PATCH_VERSION}.${GEOTIFF_REVISION_VERSION}")
 
-# Handle the QUIETLY and REQUIRED arguments and set SPATIALINDEX_FOUND to TRUE
-# if all listed variables are TRUE
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(GEOTIFF DEFAULT_MSG GEOTIFF_LIBRARY GEOTIFF_INCLUDE_DIR)
+endif()
+mark_as_advanced(GEOTIFF_LIBRARY GEOTIFF_INCLUDE_DIR)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(GeoTIFF
+                                  FOUND_VAR GEOTIFF_FOUND
+                                  REQUIRED_VARS GEOTIFF_LIBRARY GEOTIFF_INCLUDE_DIR
+                                  VERSION_VAR GEOTIFF_VERSION_STRING)
+
+if(GEOTIFF_FOUND)
+    set(GEOTIFF_LIBRARIES "${GEOTIFF_LIBRARY}")
+    set(GEOTIFF_INCLUDE_DIRS "${GEOTIFF_INCLUDE_DIR}")
+    set(GeoTIFF_TARGET GEOTIFF::GEOTIFF)
+    if(NOT TARGET ${GeoTIFF_TARGET})
+        add_library(${GeoTIFF_TARGET} UNKNOWN IMPORTED)
+        set_target_properties(${GeoTIFF_TARGET} PROPERTIES
+                              INTERFACE_INCLUDE_DIRECTORIES "${GEOTIFF_INCLUDE_DIR}"
+                              IMPORTED_LINK_INTERFACE_LANGUAGES C)
+        if(EXISTS "${GEOTIFF_LIBRARY}")
+          set_target_properties(${GeoTIFF_TARGET} PROPERTIES
+            IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+            IMPORTED_LOCATION "${GEOTIFF_LIBRARY}")
+        endif()
+        if(EXISTS "${GEOTIFF_LIBRARY_RELEASE}")
+          set_property(TARGET ${GeoTIFF_TARGET} APPEND PROPERTY
+            IMPORTED_CONFIGURATIONS RELEASE)
+          set_target_properties(${GeoTIFF_TARGET} PROPERTIES
+            IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+            IMPORTED_LOCATION_RELEASE "${GEOTIFF_LIBRARY_RELEASE}")
+        endif()
+        if(EXISTS "${GEOTIFF_LIBRARY_DEBUG}")
+          set_property(TARGET ${GeoTIFF_TARGET} APPEND PROPERTY
+            IMPORTED_CONFIGURATIONS DEBUG)
+          set_target_properties(${GeoTIFF_TARGET} PROPERTIES
+            IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C"
+            IMPORTED_LOCATION_DEBUG "${GEOTIFF_LIBRARY_DEBUG}")
+        endif()
+    endif()
+endif()
+
